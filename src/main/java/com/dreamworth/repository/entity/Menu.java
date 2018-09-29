a8a6d9e45e20d13558afad94859c84a4a7db1ce0
dreamworth <dreamworth@dreamworth-PC>
2018-09-29 12:41:50 +0530

    Menu file is updated.

--- src/main/java/com/dreamworth/repository/entity/Menu.java
+++ src/main/java/com/dreamworth/repository/entity/Menu.java
@@ -10,5 +10,16 @@ package com.dreamworth.repository.entity;
 public class Menu {
 
 	private int id;
+	private String name;
+	private int open_new_tab;
+	private int parent_id;
+	private int page_id;
+	private String link;
+	private String menu_type;
+	private int template_id;
+	private int company_id;
+	private int serial;
+	private int published;
+	private int createdby;
 	
 }
